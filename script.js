3a6e1c930d1ee51866b8c8ea4a04a34cc2b797c4
Rahil Abdul <153207425+Rahil399@users.noreply.github.com>
2025-04-15 21:44:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function convertToUpperAndAlert() {
-  const input = document.querySelector("#fname").value;
-  alert(input.toUpperCase());
-}
+document.addEventListener("DOMContentLoaded", function () {
+  function convertToUpperAndAlert() {
+    const input = document.querySelector("#fname").value;
+    alert(input.toUpperCase());
+  }
+
+  document.querySelector("#convertBtn").addEventListener("click", convertToUpperAndAlert);
+});
+
