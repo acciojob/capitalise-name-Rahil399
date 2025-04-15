24f04046925a7a7dff2f07f1f687bfdde0732e0c
Rahil Abdul <153207425+Rahil399@users.noreply.github.com>
2025-04-15 21:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   function convertToUpperAndAlert() {
     const input = document.querySelector("#fname").value;
+    
+    // Log the input to see what is being captured
+    console.log("Captured input:", input);
+
+    // Convert the input to uppercase and show in an alert
     alert(input.toUpperCase());
   }
 
   document.querySelector("#convertBtn").addEventListener("click", convertToUpperAndAlert);
 });
-
+srgkgr
